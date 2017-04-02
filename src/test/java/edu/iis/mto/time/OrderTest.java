b4f5dc230cca6800c6420ffc34f76bda844fe3b1
Gos.Konrad <194948@edu.p.lodz.pl>
2017-04-02 21:17:34 +0200

    + checkIfOrderNotExpired test - not working yet

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,6 +1,11 @@
 package edu.iis.mto.time;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by Konrad Gos on 02.04.2017.
@@ -8,6 +13,15 @@ import org.junit.Before;
 public class OrderTest {
     @Before
     public void setUp() throws Exception {
+    }
+
+    @Test
+    public void checkIfOrderNotExpired() {
+        Order order = new Order(new SystemTimeProvider());
+        order.submit();
+        order.confirm();
+        order.realize();
+        assertThat(order.getOrderState(), is(equalTo(Order.State.REALIZED)));
     }
 
 }
