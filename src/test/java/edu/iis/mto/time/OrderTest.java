e4a4082220fe92fe2839f0cdd823b8f6a98a4ea4
Gos.Konrad <194948@edu.p.lodz.pl>
2017-04-02 21:03:21 +0200

    + OrderTest class

---
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -0,0 +1,13 @@
+package edu.iis.mto.time;
+
+import org.junit.Before;
+
+/**
+ * Created by Konrad Gos on 02.04.2017.
+ */
+public class OrderTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+}
