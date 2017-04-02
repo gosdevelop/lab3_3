a958109eecbf580ed28e47176121ad2607df4eef
Gos.Konrad <194948@edu.p.lodz.pl>
2017-04-02 20:53:46 +0200

    + CustomTimeProvider class

---
+++ src/main/java/edu/iis/mto/time/CustomTimeProvider.java
@@ -0,0 +1,11 @@
+package edu.iis.mto.time;
+
+/**
+ * Created by Konrad Gos on 02.04.2017.
+ */
+public class CustomTimeProvider implements TimeInterface {
+    @Override
+    public long currentTimeMillis() {
+        return System.currentTimeMillis() +  (24 * 60 * 60 * 1000);
+    }
+}
